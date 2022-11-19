6e6acfadd6b2a4715289b279535df67293a29b41
Dougal Scott <dougal.scott@gmail.com>
2022-11-19 18:10:15 +1100

    Move Building to a separate file

--- Celemp/Player.cs
+++ Celemp/Player.cs
@@ -250,6 +250,9 @@ namespace Celemp
                 case CommandOrder.CONTRACT_TRACTOR:
                     Cmd_Contract_Tractor(cmd);
                     break;
+                case CommandOrder.BUILD_HYPER:
+                    Cmd_Build_Hyperdrive(cmd);
+                    break;
                 default:
                     Console.WriteLine($"Command not implemented {cmd.cmdstr}");
                     break;
@@ -257,51 +260,7 @@ namespace Celemp
             messages.Add(String.Join(": ", results));
         }
 
-        public void Earth_Build_Cargo(Command cmd)
-        {
-            Ship ship = galaxy!.ships[cmd.numbers["ship"]];
-            Planet plan = galaxy.planets[ship.planet];
-            int amount = cmd.numbers["amount"];
-            results = new()
-            {
-                cmd.cmdstr.ToUpper()
-            };
 
-            amount = CheckShipOre(amount, ship, 1, 1);
-            amount = CheckIndustry(amount, plan, 1);
-
-            plan.ind_left -= amount;
-            ship.carrying["1"] -= amount;
-            ship.cargo += amount;
-            int cost = amount * cmd.numbers["bid"];
-            SpendEarthCredit(cost);
-            results.Add($"Built {amount} cargo units at {cmd.numbers["bid"]} for {cost}");
-            messages.Add(String.Join(": ", results));
-        }
-
-        public void Earth_Build_Fighter(Command cmd)
-        {
-            Ship ship = galaxy!.ships[cmd.numbers["ship"]];
-            Planet plan = galaxy.planets[ship.planet];
-            results = new()
-            {
-                cmd.cmdstr.ToUpper()
-            };
-
-            int amount = cmd.numbers["amount"];
-            amount = CheckShipOre(amount, ship, 1, 2);
-            amount = CheckShipOre(amount, ship, 1, 3);
-            amount = CheckIndustry(amount, plan, 2);
-
-            plan.ind_left -= amount * 2;
-            ship.carrying["2"] -= amount;
-            ship.carrying["3"] -= amount;
-            ship.fighter += amount;
-            int cost = amount * cmd.numbers["bid"];
-            SpendEarthCredit(cost);
-            results.Add($"Built {amount} fighter units at {cmd.numbers["bid"]} for {cost}");
-            messages.Add(String.Join(": ", results));
-        }
 
         public void SpendEarthCredit(int cost)
         {
@@ -312,53 +271,6 @@ namespace Celemp
                 score -= debt * galaxy!.earthMult;
                 earthCredit += debt;
             }
-        }
-
-        public void Earth_Build_Shield(Command cmd)
-        {
-            Ship ship = galaxy!.ships[cmd.numbers["ship"]];
-            Planet plan = galaxy.planets[ship.planet];
-            results = new()
-            {
-                cmd.cmdstr.ToUpper()
-            };
-
-            int amount = cmd.numbers["amount"];
-            amount = CheckShipOre(amount, ship, 1, 5);
-            amount = CheckShipOre(amount, ship, 1, 6);
-            amount = CheckIndustry(amount, plan, 2);
-
-            plan.ind_left -= amount * 2;
-            ship.carrying["5"] -= amount;
-            ship.carrying["6"] -= amount;
-
-            ship.shield += amount;
-            int cost = amount * cmd.numbers["bid"];
-            SpendEarthCredit(cost);
-            results.Add($"Built {amount} shield units at {cmd.numbers["bid"]} for {cost}");
-            messages.Add(String.Join(": ", results));
-        }
-
-        public void Earth_Build_Tractor(Command cmd)
-        {
-            Ship ship = galaxy!.ships[cmd.numbers["ship"]];
-            Planet plan = galaxy.planets[ship.planet];
-            results = new()
-            {
-                cmd.cmdstr.ToUpper()
-            };
-
-            int amount = cmd.numbers["amount"];
-            amount = CheckShipOre(amount, ship, 2, 7);
-            amount = CheckIndustry(amount, plan, 2);
-
-            plan.ind_left -= amount * 2;
-            ship.carrying["7"] -= amount;
-            ship.tractor += amount;
-            int cost = amount * cmd.numbers["bid"];
-            SpendEarthCredit(cost);
-            results.Add($"Built {amount} tractor units at {cmd.numbers["bid"]} for {cost}");
-            messages.Add(String.Join(": ", results));
         }
 
         // Contracts are weird as they get resolved in contractor order
@@ -472,10 +384,7 @@ namespace Celemp
 
         public void System_ResolveContracts(Command cmd)
         {
-            galaxy!.ResolveContracts("cargo");
-            galaxy!.ResolveContracts("fighter");
-            galaxy!.ResolveContracts("shield");
-            galaxy!.ResolveContracts("tractor");
+            galaxy!.ResolveContracts();
         }
 
         public void System_ResolveAttacks(Command cmd)
@@ -551,135 +460,6 @@ namespace Celemp
             galaxy.planets[ship.planet].ore[oretype] += amount;
         }
 
-        public void Cmd_BuildIndustry(Command cmd)
-        {
-            Planet plan = galaxy!.planets[cmd.numbers["planet"]];
-            if (!CheckPlanetOwnership(plan, cmd))
-                return;
-            int amount = cmd.numbers["amount"];
-            amount = CheckIndustry(amount, plan, 10);
-            amount = CheckOre(amount, plan, 5, 8);
-            amount = CheckOre(amount, plan, 5, 9);
-
-            plan.ind_left -= amount * 10;
-            plan.ore[8] -= amount * 5;
-            plan.ore[9] -= amount * 5;
-            plan.industry += amount;
-            results.Add($"Built {amount} industry");
-        }
-
-        public void Cmd_BuildSpacemine(Command cmd)
-        {
-            Planet plan = galaxy!.planets[cmd.numbers["planet"]];
-            if (!CheckPlanetOwnership(plan, cmd))
-                return;
-            int amount = cmd.numbers["amount"];
-            int oretype = cmd.numbers["oretype"];
-            amount = CheckIndustry(amount, plan, 1);
-            amount = CheckOre(amount, plan, 1, oretype);
-
-            plan.ind_left -= amount;
-            plan.ore[oretype] -= amount;
-            plan.spacemines += amount;
-            results.Add($"Built {amount} spacemines");
-        }
-
-        public void Cmd_BuildPDU(Command cmd)
-        {
-            Planet plan = galaxy!.planets[cmd.numbers["planet"]];
-            if (!CheckPlanetOwnership(plan, cmd))
-                return;
-            int amount = cmd.numbers["amount"];
-            amount = CheckIndustry(amount, plan, 1);
-            amount = CheckOre(amount, plan, 1, 4);
-
-            plan.ind_left -= amount;
-            plan.ore[4] -= amount;
-            plan.pdu += amount;
-            results.Add($"Built {amount} PDU");
-        }
-
-        public void Cmd_BuildCargo(Command cmd)
-        {
-            // Build Cargo units on a ship
-            // A cargo takes 1 industry and one ore type 1
-            Ship ship = galaxy!.ships[cmd.numbers["ship"]];
-            Planet plan = galaxy.planets[ship.planet];
-            if (!CheckShipOwnership(ship, cmd))
-                return;
-            if (!CheckPlanetOwnership(plan, cmd))
-                return;
-            int amount = cmd.numbers["amount"];
-            amount = CheckIndustry(amount, plan, 1);
-            amount = CheckOre(amount, plan, 1, 1);
-
-            plan.ind_left -= amount;
-            plan.ore[1] -= amount;
-            ship.cargo += amount;
-            results.Add($"Built {amount} cargo");
-        }
-
-        public void Cmd_BuildFighter(Command cmd)
-        // Build Fighter units on a ship
-        // A fighter takes 2 industry and one each of ore 2 and 3
-        {
-            Ship ship = galaxy!.ships[cmd.numbers["ship"]];
-            Planet plan = galaxy.planets[ship.planet];
-            if (!CheckShipOwnership(ship, cmd))
-                return;
-            if (!CheckPlanetOwnership(plan, cmd))
-                return;
-            int amount = cmd.numbers["amount"];
-            amount = CheckIndustry(amount, plan, 2);
-            amount = CheckOre(amount, plan, 1, 2);
-            amount = CheckOre(amount, plan, 1, 3);
-
-            plan.ind_left -= amount * 2;
-            plan.ore[2] -= amount;
-            plan.ore[3] -= amount;
-            ship.fighter += amount;
-            results.Add($"Built {amount} fighter");
-        }
-
-        public void Cmd_BuildTractor(Command cmd)
-        {
-            Ship ship = galaxy!.ships[cmd.numbers["ship"]];
-            Planet plan = galaxy.planets[ship.planet];
-            if (!CheckShipOwnership(ship, cmd))
-                return;
-            if (!CheckPlanetOwnership(plan, cmd))
-                return;
-            int amount = cmd.numbers["amount"];
-            amount = CheckIndustry(amount, plan, 2);
-            amount = CheckOre(amount, plan, 2, 7);
-
-            plan.ind_left -= amount * 2;
-            plan.ore[7] -= amount * 2;
-            ship.tractor += amount;
-            results.Add($"Built {amount} tractor");
-        }
-
-        public void Cmd_BuildShield(Command cmd)
-        {
-            Ship ship = galaxy!.ships[cmd.numbers["ship"]];
-            Planet plan = galaxy.planets[ship.planet];
-            if (!CheckShipOwnership(ship, cmd))
-                return;
-            if (!CheckPlanetOwnership(plan, cmd))
-                return;
-            int amount = cmd.numbers["amount"];
-            amount = CheckIndustry(amount, plan, 2);
-            amount = CheckOre(amount, plan, 1, 5);
-            amount = CheckOre(amount, plan, 1, 6);
-
-            plan.ind_left -= amount * 2;
-            plan.ore[5] -= amount;
-            plan.ore[6] -= amount;
-
-            ship.shield += amount;
-            results.Add($"Built {amount} shields");
-        }
-
         public int CheckIndustry(int amount, Planet plan, int scale)
         {
             if (plan.ind_left < amount * scale)
@@ -709,24 +489,6 @@ namespace Celemp
                 results.Add($"Insufficient Ore {oretype}");
             }
             return amount;
-        }
-
-        public void Cmd_BuildMine(Command cmd)
-        {
-            Planet plan = galaxy!.planets[cmd.numbers["planet"]];
-            if (!CheckPlanetOwnership(plan, cmd))
-                return;
-            int amount = cmd.numbers["amount"];
-            int oretype = cmd.numbers["oretype"];
-            amount = CheckIndustry(amount, plan, 10);
-            amount = CheckOre(amount, plan, 5, 8);
-            amount = CheckOre(amount, plan, 5, 9);
-
-            plan.ind_left -= amount * 10;
-            plan.ore[8] -= amount * 5;
-            plan.ore[9] -= amount * 5;
-            plan.mine[oretype] += amount;
-            results.Add($"Built {amount} mine type {oretype}");
         }
 
         public void Cmd_GiftShip(Command cmd)
---
+++ CelempTest/BuildingTest.cs
@@ -0,0 +1,300 @@
+﻿namespace CelempTest;
+using Celemp;
+using static Celemp.Constants;
+
+[TestClass]
+public class BuildingTest
+{
+    [TestMethod]
+    public void Cmd_BuildMine()
+    {
+        Galaxy g = new();
+        Planet pt = new();
+        Player pr = new("Max");
+        g.planets[235] = pt;
+        pt.owner = 2;
+        pr.InitPlayer(g, 2);
+        pt.industry = 50;
+        pt.ind_left = 50;
+        pt.ore[8] = 40;
+        pt.ore[9] = 20;
+        pt.mine[8] = 0;
+
+        Command cmd = new("335b5m8", 2);
+        pr.ProcessCommand(cmd);
+        pr.OutputLog();
+
+        Assert.IsTrue(pr.messages[0].Contains("Insufficient Ore 9"));
+
+        // Should be constrained to building 4
+        Assert.AreEqual(50 - 4 * 10, pt.ind_left);
+        Assert.AreEqual(40 - 4 * 5, pt.ore[8]);
+        Assert.AreEqual(20 - 4 * 5, pt.ore[9]);
+        Assert.AreEqual(4, pt.mine[8]);
+    }
+
+    [TestMethod]
+    public void Cmd_BuildCargo()
+    {
+        Galaxy g = new();
+        Planet plan = new();
+        Player plr = new();
+        Ship s = g.InitShip();
+
+        g.planets[1] = plan;
+        plr.InitPlayer(g, 1);
+
+        plan.owner = 1;
+        plan.industry = 20;
+        plan.ind_left = 20;
+        plan.ore[1] = 20;
+        s.owner = 1;
+        s.planet = 1;
+
+        Command cmd = new("S100B10C", 1);
+        plr.ProcessCommand(cmd);
+        plr.OutputLog();
+
+        Assert.AreEqual(10, s.cargo);
+        Assert.AreEqual(10, s.CargoLeft());
+        Assert.AreEqual(20 - 10, plan.ind_left);
+        Assert.AreEqual(20 - 10, plan.ore[1]);
+    }
+
+    [TestMethod]
+    public void Cmd_BuildFighter()
+    {
+        Galaxy g = new();
+        Planet plan = new();
+        Player plr = new();
+        Ship s = g.InitShip();
+
+        g.planets[1] = plan;
+        plr.InitPlayer(g, 1);
+
+        plan.owner = 1;
+        plan.industry = 20;
+        plan.ind_left = 20;
+        plan.ore[2] = 10;
+        plan.ore[3] = 8;
+        s.owner = 1;
+        s.planet = 1;
+
+        Command cmd = new("S100B10F", 1);
+        plr.ProcessCommand(cmd);
+        plr.OutputLog();
+
+        Assert.IsTrue(plr.messages[0].Contains("Insufficient Ore 3"));
+        Assert.AreEqual(8, s.fighter);
+        Assert.AreEqual(20 - (8 * 2), plan.ind_left);
+        Assert.AreEqual(10 - 8, plan.ore[2]);
+        Assert.AreEqual(8 - 8, plan.ore[3]);
+    }
+
+
+    [TestMethod]
+    public void Cmd_BuildTractor()
+    {
+        Galaxy g = new();
+        Planet plan = new();
+        Player plr = new();
+        Ship s = new();
+
+        g.planets[1] = plan;
+        g.ships[0] = s;
+        plr.InitPlayer(g, 1);
+
+        plan.owner = 1;
+        plan.industry = 20;
+        plan.ind_left = 20;
+        plan.ore[7] = 15;
+        s.owner = 1;
+        s.planet = 1;
+        s.SetGalaxy(g);
+
+        Command cmd = new("S100B10T", 1);
+        plr.ProcessCommand(cmd);
+        plr.OutputLog();
+
+        Assert.IsTrue(plr.messages[0].Contains("Insufficient Ore 7"));
+
+        Assert.AreEqual(7, s.tractor);
+        Assert.AreEqual(20 - (7 * 2), plan.ind_left);
+        Assert.AreEqual(15 - (7 * 2), plan.ore[7]);
+    }
+
+    [TestMethod]
+    public void Cmd_BuildShield()
+    {
+        Galaxy g = new();
+        Planet plan = new();
+        Player plr = new();
+        Ship s = g.InitShip();
+
+        g.planets[1] = plan;
+        plr.InitPlayer(g, 1);
+
+        plan.owner = 1;
+        plan.industry = 20;
+        plan.ind_left = 12;
+        plan.ore[5] = 20;
+        plan.ore[6] = 20;
+
+        s.owner = 1;
+        s.planet = 1;
+
+        Command cmd = new("S100B10S", 1);
+        plr.ProcessCommand(cmd);
+        plr.OutputLog();
+
+        Assert.IsTrue(plr.messages[0].Contains("Insufficient Industry"));
+
+        Assert.AreEqual(6, s.shield);
+        Assert.AreEqual(12 - (6 * 2), plan.ind_left);
+        Assert.AreEqual(20 - 6, plan.ore[5]);
+        Assert.AreEqual(20 - 6, plan.ore[6]);
+    }
+
+    [TestMethod]
+    public void Cmd_Planet_Build_Industry()
+    {
+        Galaxy g = new();
+        Planet plan = new();
+        Player plr = new("Max");
+
+        g.players[1] = plr;
+        g.planets[100] = plan;
+
+        plr.InitPlayer(g, 1);
+        plan.number = 100;
+        plan.owner = 1;
+        plan.industry = 50;
+        plan.ore[8] = 30;
+        plan.ore[9] = 30;
+        plan.setGalaxy(g);
+        plan.InitialiseTurn();
+
+        Command cmd = new("200B10I", 1);
+        plr.ProcessCommand(cmd);
+        plr.OutputLog();
+
+        Assert.AreEqual(50 + 5, plan.industry);
+        Assert.AreEqual(0, plan.ind_left);
+        Assert.AreEqual(30 - (5 * 5), plan.ore[8]);
+        Assert.AreEqual(30 - (5 * 5), plan.ore[9]);
+    }
+
+    [TestMethod]
+    public void Cmd_Planet_Build_PDU()
+    {
+        Galaxy g = new();
+        Planet plan = new();
+        Player plr = new("Max");
+
+        g.players[1] = plr;
+        g.planets[100] = plan;
+
+        plr.InitPlayer(g, 1);
+        plan.number = 100;
+        plan.owner = 1;
+        plan.industry = 50;
+        plan.ore[4] = 30;
+        plan.pdu = 0;
+        plan.setGalaxy(g);
+        plan.InitialiseTurn();
+
+        Command cmd = new("200B10D", 1);
+        plr.ProcessCommand(cmd);
+        plr.OutputLog();
+
+        Assert.AreEqual(50 - 10, plan.ind_left);
+        Assert.AreEqual(30 - (10 * 1), plan.ore[4]);
+        Assert.AreEqual(10, plan.pdu);
+    }
+
+
+    [TestMethod]
+    public void Cmd_Planet_Build_Spacemines()
+    {
+        Galaxy g = new();
+        Planet plan = new();
+        Player plr = new("Max");
+
+        g.players[1] = plr;
+        g.planets[100] = plan;
+
+        plr.InitPlayer(g, 1);
+        plan.number = 100;
+        plan.owner = 1;
+        plan.industry = 50;
+        plan.ore[2] = 30;
+        plan.pdu = 0;
+        plan.setGalaxy(g);
+        plan.InitialiseTurn();
+
+        Command cmd = new("200B10S2", 1);
+        plr.ProcessCommand(cmd);
+        plr.OutputLog();
+
+        Assert.AreEqual(50 - 10, plan.ind_left);
+        Assert.AreEqual(30 - (10 * 1), plan.ore[2]);
+        Assert.AreEqual(10, plan.spacemines);
+    }
+
+    [TestMethod]
+    public void Cmd_Planet_Build_Hyperdrive()
+    {
+        Galaxy g = new();
+        Planet plan = new();
+        Player plr = new("Max");
+        Ship s = g.InitShip();
+
+        int fighter = 1;
+        int cargo = 2;
+        int tractor = 3;
+        int shield = 4;
+
+        g.players[1] = plr;
+        g.planets[100] = plan;
+
+        plr.InitPlayer(g, 1);
+        plan.number = 100;
+        plan.owner = 1;
+        plan.industry = 80;
+        plan.ore[1] = 30;
+        plan.ore[2] = 30;
+        plan.ore[3] = 30;
+        plan.ore[4] = 30;
+        plan.ore[5] = 30;
+        plan.ore[6] = 30;
+        plan.ore[7] = 30;
+        plan.setGalaxy(g);
+        plan.InitialiseTurn();
+
+        Command cmd = new($"200B1H{fighter}/{cargo}/{tractor}/{shield}", 1);
+        plr.ProcessCommand(cmd);
+        plr.OutputLog();
+
+        Assert.AreEqual(80 - 40 - fighter * 2 - cargo - tractor * 2 - shield * 2, plan.ind_left);
+        Assert.AreEqual(30 - cargo, plan.ore[1]);
+        Assert.AreEqual(30 - fighter, plan.ore[2]);
+        Assert.AreEqual(30 - fighter, plan.ore[3]);
+        Assert.AreEqual(30 - 10, plan.ore[4]);
+        Assert.AreEqual(30 - 10 - shield, plan.ore[5]);
+        Assert.AreEqual(30 - 10 - shield, plan.ore[6]);
+        Assert.AreEqual(30 - 10 - tractor * 2, plan.ore[7]);
+
+        Assert.AreEqual(1, plan.ShipsOrbitting().Count);
+
+        foreach (var ship in plan.ShipsOrbitting())
+        {
+            Assert.AreEqual(fighter, ship.fighter);
+            Assert.AreEqual(cargo, ship.cargo);
+            Assert.AreEqual(tractor, ship.tractor);
+            Assert.AreEqual(shield, ship.shield);
+            Assert.AreEqual(1, ship.number);
+        }
+
+    }
+}
+
